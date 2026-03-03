12e2d7ff0b1da9437fc1fcf1c2e72a426dcc47e0
dl Yang <hcksfch@163.com>
2026-03-04 00:01:48 +0800

    change vitest config

--- packages/components/vitest.config.ts
+++ packages/components/vitest.config.ts
@@ -6,12 +6,12 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
-  // resolve: {
-  //   alias: {
-  //     // 将包名 toy-elementhh 指向 core 源码
-  //     "toy-elementhh": resolve(__dirname, "../core/index.ts"),
-  //   },
-  // },
+  resolve: {
+    alias: {
+      // 将包名 toy-elementhh 指向 core 源码
+      "toy-elementhh": resolve(__dirname, "../core/index.ts"),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
